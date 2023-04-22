64ff7d30cd326fb4c0652b271f71dbe8bf09eae2
Nghia Hoang <119391745+woo-negg@users.noreply.github.com>
2023-04-22 16:53:31 +0000

    remove unneeded stuff

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-from PIL import Image, ImageDraw, ImageOps
+from PIL import Image, ImageDraw
 import os
 import math
 
 logo_path = 'logo.png'
-output_gif_path = 'animated_logo_with_loading_circle.gif'
+output_gif_path = 'animated_logo_with_dotted_circle.gif'
 
 frame_count = 24
 frame_duration = 100  # milliseconds
 
-def create_blinking_frame(logo, visible, angle):
+def create_dotted_circle_frame(logo, angle):
     frame = Image.new('RGBA', logo.size, (255, 255, 255, 0))
+    frame.paste(logo, (0, 0), logo)
 
-    if visible:
-        frame.paste(logo, (0, 0), logo)
-
-    circle_radius = int(min(logo.size) * 0.4)
-    circle_thickness = int(circle_radius * 0.1)
+    circle_radius = int(min(logo.size) * 0.45)
     circle_center = (logo.size[0] // 2, logo.size[1] // 2)
+    dot_radius = int(circle_radius * 0.1)
+    dot_count = 12
 
     circle = Image.new('RGBA', logo.size, (255, 255, 255, 0))
     draw = ImageDraw.Draw(circle)
-    draw.ellipse(
-        (
-            circle_center[0] - circle_radius,
-            circle_center[1] - circle_radius,
-            circle_center[0] + circle_radius,
-            circle_center[1] + circle_radius
-        ),
-        outline=(0, 0, 0, 255),
-        width=circle_thickness
-    )
 
-    circle = circle.rotate(angle, resample=Image.BICUBIC, center=circle_center)
+    for i in range(dot_count):
+        dot_angle = (360 / dot_count) * i + angle
+        dot_x = circle_center[0] + circle_radius * math.cos(math.radians(dot_angle)) - dot_radius
+        dot_y = circle_center[1] + circle_radius * math.sin(math.radians(dot_angle)) - dot_radius
+        draw.ellipse(
+            (
+                dot_x, dot_y,
+                dot_x + 2 * dot_radius, dot_y + 2 * dot_radius
+            ),
+            fill=(0, 0, 0, 255)
+        )
+
     frame = Image.alpha_composite(frame, circle)
 
     return frame
@@ -41,7 +41,7 @@ logo = Image.open(logo_path).convert('RGBA')
 frames = []
 for i in range(frame_count):
     angle = (360 / frame_count) * i
-    frame = create_blinking_frame(logo, i % 2 == 0, angle)
+    frame = create_dotted_circle_frame(logo, angle)
     frames.append(frame)
 
 frames[0].save(
